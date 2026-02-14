837156deaea4fa896bde3ca4674493c840ac3a72
sandip tamang <tamangsandip609@gmail.com>
2026-02-14 16:42:08 +0545

    feat: converting django files into react component improved ui/ux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import Home from './pages/Home';
-import { Routes, Route } from 'react-router';
-import Login from './pages/Login';
+// import Home from './pages/Home';
+// import { Routes, Route } from 'react-router';
+// import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-white font-sans antialiased">
-      <Routes>
+      <Dashboard />
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/facts" element={<Facts />} /> */}
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
