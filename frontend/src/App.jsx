1740f0eb7b512194fd41ecbd57b7c4d43cb51b16
sandip tamang <tamangsandip609@gmail.com>
2026-02-14 13:11:22 +0545

    feat(frontend): add React Router and test basic routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-// import Home from './pages/Home';
+import Home from './pages/Home';
+import { Routes, Route } from 'react-router';
 import Login from './pages/Login';
 
-function App() {
+export default function App() {
   return (
     <div className="min-h-screen bg-white font-sans antialiased">
-      {/* <Home /> */}
-      <Login />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/facts" element={<Facts />} /> */}
+      </Routes>
     </div>
   );
 }
-
-export default App;
